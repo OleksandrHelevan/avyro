54bbf12e8ada0281d8f9e65901302856b6cc2443
Doomik <dronten7@gmail.com>
2026-05-06 20:29:50 +0300

    Add new features for return of rewards

--- src/web-api/modules/users_module/application/dto/PatientResponse.py
+++ src/web-api/modules/users_module/application/dto/PatientResponse.py
@@ -1,7 +1,12 @@
 from pydantic import BaseModel, Field, ConfigDict
-from typing import Optional
+from typing import Optional, List
 from datetime import datetime
 
+class RewardItemResponse(BaseModel):
+    id: str
+    title: str
+    description: Optional[str] = None
+    issued_at: Optional[str] = None
 
 class PatientResponse(BaseModel):
     model_config = ConfigDict(populate_by_name=True)
@@ -14,3 +19,7 @@ class PatientResponse(BaseModel):
     avatarUrl: Optional[str] = None
     createdAt: datetime
     lastLoginAt: Optional[datetime] = None
+    rewards: List[RewardItemResponse] = [] # Додаємо це поле
+
+
+
