aeba04c7f83fda85a404156b82ac2fb3681b07e7
Doomik <dronten7@gmail.com>
2026-06-03 01:08:25 +0300

    Lists of doctors are fixed

--- src/web-api/modules/users_module/api/DoctorController.py
+++ src/web-api/modules/users_module/api/DoctorController.py
@@ -1,11 +1,9 @@
 from fastapi import APIRouter, Depends, HTTPException, status, Query
 from config.dependencies import get_doctor_service, get_patient_service
 from config.security import get_current_user
-
 from typing import List
 from config.logging_config import logger
 from modules.users_module.application.dto.DoctorListItemResponse import DoctorListItemResponse
-
 from modules.users_module.application.services.DoctorService import DoctorService
 from modules.users_module.application.dto.DoctorProfile import (
     DoctorProfileUpdateRequest,
@@ -13,6 +11,7 @@ from modules.users_module.application.dto.DoctorProfile import (
 )
 
 router = APIRouter(prefix="/users/doctors", tags=["Doctors"])
+
 
 def require_doctor_role(user: dict = Depends(get_current_user)) -> dict:
     if user.get("role") != "DOCTOR":
@@ -22,6 +21,7 @@ def require_doctor_role(user: dict = Depends(get_current_user)) -> dict:
         )
     return user
 
+
 @router.get("/{user_id}", response_model=DoctorProfileResponse)
 def get_doctor_by_id(
     user_id: str,
@@ -30,13 +30,14 @@ def get_doctor_by_id(
 ):
     return service.get_doctor_by_id(user_id)
 
+
 @router.get("", response_model=List[DoctorListItemResponse])
 def get_doctors(
-    current_user: dict = Depends(get_current_user), # Додано сюди теж
     service=Depends(get_patient_service)
 ):
     logger.info("Fetching doctors list.")
     return service.get_doctors_list()
+
 
 @router.patch("", response_model=DoctorProfileResponse)
 def patch_doctor_profile(
@@ -46,11 +47,3 @@ def patch_doctor_profile(
 ):
     user_id = current_user.get("sub")
     return service.patch_doctor_profile(user_id, profile_data)
-
-
-@router.get("", response_model=List[DoctorListItemResponse])
-def get_doctors(
-    service=Depends(get_patient_service)
-):
-    logger.info("Fetching doctors list.")
-    return service.get_doctors_list()
